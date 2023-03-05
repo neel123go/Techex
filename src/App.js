32fc552a9e692f4cd1b90058977315f7032c2297
neel123go <ayounpaul1029@gmail.com>
2023-03-06 01:55:22 +0600

    Change file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Pages/shared/Header/Header';
+import Header from './Components/Header';
 
 function App() {
   return (
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Logo from '../../../assets/img/logo.png';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/img/logo.png';
 
 const Header = () => {
     const [menu, setMenu] = useState(false);
@@ -12,42 +13,42 @@ const Header = () => {
                 <div className="row align-items-center">
                     <div className="col-6 col-sm-5 col-xl-2">
                         <div className="logo">
-                            <a href="index.html">
+                            <Link to="index.html">
                                 <img src={Logo} alt="logo" />
-                            </a>
+                            </Link>
                         </div>
                     </div>
                     <div className="col-xl-7 d-none d-xl-block">
                         <div className="main-menu">
                             <ul>
-                                <li><a href="#">Home <i className="fas fa-angle-down"></i></a>
+                                <li><Link href="#">Home <i className="fas fa-angle-down"></i></Link>
                                     <ul className="sub-menu">
-                                        <li><a href="index.html">home 1</a></li>
-                                        <li><a href="index-2.html">home 2</a></li>
-                                        <li><a href="index-3.html">home 3</a></li>
-                                        <li><a href="index-4.html">home 4</a></li>
-                                        <li><a href="index-5.html">home 5</a></li>
-                                        <li><a href="index-6.html">home 6</a></li>
+                                        <li><Link to="">home 1</Link></li>
+                                        <li><Link to="">home 2</Link></li>
+                                        <li><Link to="">home 3</Link></li>
+                                        <li><Link to="">home 4</Link></li>
+                                        <li><Link to="">home 5</Link></li>
+                                        <li><Link to="">home 6</Link></li>
                                     </ul>
                                 </li>
-                                <li><a href="about.html">About</a> </li>
-                                <li><a href="services.html">Services</a></li>
-                                <li><a href="#">Pages <i className="fas fa-angle-down"></i></a>
+                                <li><Link to="about.html">About</Link> </li>
+                                <li><Link to="services.html">Services</Link></li>
+                                <li><Link to="#">Pages <i className="fas fa-angle-down"></i></Link>
                                     <ul className="sub-menu">
-                                        <li><a href="cases-grid.html">case grid</a></li>
-                                        <li><a href="project-details.html">case details</a></li>
-                                        <li><a href="faq.html">faq</a></li>
-                                        <li><a href="team.html">team</a></li>
-                                        <li><a href="case-2.html">case two</a></li>
+                                        <li><Link to="">case grid</Link></li>
+                                        <li><Link to="">case details</Link></li>
+                                        <li><Link to="">faq</Link></li>
+                                        <li><Link to="">team</Link></li>
+                                        <li><Link to="">case two</Link></li>
                                     </ul>
                                 </li>
-                                <li><a href="news.html">News</a></li>
-                                <li><a href="contact.html">Contact</a></li>
+                                <li><Link to="news.html">News</Link></li>
+                                <li><Link to="contact.html">Contact</Link></li>
                             </ul>
                         </div>
                     </div>
                     <div className="col-xl-3 col-sm-6 d-none d-sm-block pl-xl-0 text-right">
-                        <a href="contact.html" className="theme-btn">Consultancy <i className="fas fa-arrow-right"></i></a>
+                        <Link to="contact.html" className="theme-btn">Consultancy <i className="fas fa-arrow-right"></i></Link>
                     </div>
                     <div className="mobile-nav-bar d-block col-sm-1 col-6 d-xl-none">
                         <div className="mobile-nav-wrap">
@@ -64,7 +65,7 @@ const Header = () => {
 
                                 <nav className="sidebar-nav">
                                     <ul className="metismenu" id="mobile-menu">
-                                        <li><a className="has-arrow" href="#" onClick={() => setHomeSubMenu(!homeSubMenu)}>Homes</a>
+                                        <li><Link className="has-arrow" to="#" onClick={() => setHomeSubMenu(!homeSubMenu)}>Homes</Link>
                                             {homeSubMenu ? <ul className="sub-menu">
                                                 <li><a href="index.html">Homepage 1</a></li>
                                                 <li><a href="index-2.html">Homepage 2</a></li>
@@ -74,27 +75,27 @@ const Header = () => {
                                                 <li><a href="index-6.html">Homepage 6</a></li>
                                             </ul> : ''}
                                         </li>
-                                        <li><a href="about.html">about</a></li>
-                                        <li><a href="services.html">services</a></li>
+                                        <li><Link to="about.html">about</Link></li>
+                                        <li><Link to="services.html">services</Link></li>
                                         <li>
-                                            <a className="has-arrow" onClick={() => setPageSubMenu(!pageSubMenu)} href="#">Pages</a>
+                                            <Link className="has-arrow" onClick={() => setPageSubMenu(!pageSubMenu)} to="#">Pages</Link>
                                             {pageSubMenu ? <ul className="sub-menu">
-                                                <li><a href="faq.html">faq</a></li>
-                                                <li><a href="services-details.html">services details</a></li>
-                                                <li><a href="team.html">Team</a></li>
-                                                <li><a href="cases-grid.html">Case Grid</a></li>
-                                                <li><a href="case-2.html">Case Grid 2</a></li>
+                                                <li><Link to="faq.html">faq</Link></li>
+                                                <li><Link to="services-details.html">services details</Link></li>
+                                                <li><Link to="team.html">Team</Link></li>
+                                                <li><Link to="cases-grid.html">Case Grid</Link></li>
+                                                <li><Link to="case-2.html">Case Grid 2</Link></li>
                                             </ul> : ''}
                                         </li>
-                                        <li><a href="news.html">News</a></li>
-                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><Link to="news.html">News</Link></li>
+                                        <li><Link to="contact.html">Contact</Link></li>
                                     </ul>
                                 </nav>
 
                                 <div className="action-bar">
-                                    <a href="mailto:modinatheme@gmail.com"><i className="fal fa-envelope-open-text"></i>info@webmail.com</a>
-                                    <a href="tel:123-456-7890"><i className="fal fa-phone"></i>987-098-098-09</a>
-                                    <a href="contact.html" className="d-btn theme-btn black">Consultancy</a>
+                                    <Link to="mailto:modinatheme@gmail.com"><i className="fal fa-envelope-open-text"></i>info@webmail.com</Link>
+                                    <Link to="tel:123-456-7890"><i className="fal fa-phone"></i>987-098-098-09</Link>
+                                    <Link to="contact.html" className="d-btn theme-btn black">Consultancy</Link>
                                 </div>
 
                             </div>
