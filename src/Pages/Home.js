ebb472ecafe6b74301e8637e2aa603bfe80e8923
neel123go <ayounpaul1029@gmail.com>
2023-03-07 12:59:32 +0600

    Popular it services section design completed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutCompany from '../Sections/AboutCompany';
 import Banner from '../Sections/Banner';
 import HowCanHelpYou from '../Sections/HowCanHelpYou';
+import PopularItServices from '../Sections/PopularItServices';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <HowCanHelpYou />
             <AboutCompany />
+            <PopularItServices />
         </>
     )
 }
---
+++ src/Sections/PopularItServices.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const PopularItServices = () => {
+    return (
+        <section class="services-wrapper service-1 section-padding section-bg">
+            <div class="container">
+                <div class="row mb-4 mb-lg-5">
+                    <div class="col-12 col-lg-12">
+                        <div class="section-title text-white text-center">
+                            <p>Popular IT services</p>
+                            <h1>Our Professional Solutions <br />
+                                For IT Business</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 col-xl-3 col-12">
+                        <div class="single-service-item">
+                            <div class="icon">
+                                <img src="assets/img/icon/sicon1.png" alt="service" />
+                            </div>
+                            <h4>Manage IT Services</h4>
+                            <p>Sed perspicias unde omnis natus error volute</p>
+                            <a href="services-details.html">learn more <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3 col-12">
+                        <div class="single-service-item">
+                            <div class="icon">
+                                <img src="assets/img/icon/sicon2.png" alt="service" />
+                            </div>
+                            <h4>Product Engineering</h4>
+                            <p>Sed perspicias unde omnis natus error volute</p>
+                            <a href="services-details.html">learn more <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3 col-12">
+                        <div class="single-service-item">
+                            <div class="icon">
+                                <img src="assets/img/icon/sicon3.png" alt="service" />
+                            </div>
+                            <h4>Web Development</h4>
+                            <p>Sed perspicias unde omnis natus error volute</p>
+                            <a href="services-details.html">learn more <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3 col-12">
+                        <div class="single-service-item">
+                            <div class="icon">
+                                <img src="assets/img/icon/sicon4.png" alt="service" />
+                            </div>
+                            <h4>Digital Solutions</h4>
+                            <p>Sed perspicias unde omnis natus error volute</p>
+                            <a href="services-details.html">learn more <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default PopularItServices;
